526ef765078ba32bce1b6bb22cf834b140e0e01a
yuriy <yuriy@debian>
2018-05-02 13:22:27 -0400

    Add POP testing func

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from imaplib import IMAP4_SSL
 import getpass
 import smtplib
 import random
+from poplib import POP3_SSL
 
 
 username = "yzhigulskiy@OPS-EXCH154-W.hostpilot.com"
@@ -9,41 +10,66 @@ host = "mail154-1.exch154.serverdata.net"
 password = "Gfhjkm951"
 msg_id = random.randint(1000000, 10000000000)
 
-print("IMAP testing")
+def send_msg():
+    print(msg_id)
+    fromaddr = "yurok15@gmail.com"
+    toaddrs = "yzhigulskiy@OPS-EXCH154-W.hostpilot.com"
+    msg = ("Subject: %s" % (msg_id))
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        if not line:
+            break
+        msg = msg + line
 
-connector = IMAP4_SSL(host)
-try:
-    connector.login(username, password)
-    print("OK - IMAPS connectivity (port 993)")
-except:
-    print("FAILED - Couldn't connect to server - BAD")
+    print("Message length is", len(msg))
 
-connector.select()
-msg = connector.search(None, 'Subject', '"testing222"')
-if msg[1] != [b'']:
-    print("OK - Sent message was found")
-else:
-    print("FAILED - Sent message was not delivered")
+    server = smtplib.SMTP('west.smtp.mx.exch154.serverdata.net')
+    server.set_debuglevel(1)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
 
-#connector.close()
-connector.logout()
 
-print(msg_id)
-fromaddr = "yurok15@gmail.com"
-toaddrs  = "yzhigulskiy@OPS-EXCH154-W.hostpilot.com"
-msg = ("Subject: %s" %(msg_id) )
-while True:
+def test_imap(username, host, password, msg_id):
+    print("IMAP testing")
+
+    connector = IMAP4_SSL(host)
     try:
-        line = input()
-    except EOFError:
-        break
-    if not line:
-        break
-    msg = msg + line
+        connector.login(username, password)
+        print("OK - IMAPS connectivity (port 993)")
+    except:
+        print("FAILED - Couldn't connect to server")
 
-print("Message length is", len(msg))
+    connector.select()
+    msg = connector.search(None, 'Subject', '"testing222"')
+    if msg[1] != [b'']:
+        print("OK - Sent message was found")
+    else:
+        print("FAILED - Sent message was not delivered")
 
-server = smtplib.SMTP('west.smtp.mx.exch154.serverdata.net')
-server.set_debuglevel(1)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+    connector.close()
+    connector.logout()
+
+
+def test_pop(username, host, password, msg_id):
+    print("POP3 testing")
+
+    connector = POP3_SSL(host)
+    try:
+        connector.user(username)
+        connector.pass_(password)
+        print("OK - POP3 connectivity (port 995)")
+    except:
+        print("FAILED - Couldn't connect to server")
+
+    if len(connector.list()[1]) > 0:
+        print("OK - INBOX not empty")
+    else:
+        print("--- - No messages in INBOX")
+
+#test_imap(username, host, password, msg_id)
+#test_pop(username, host, password, msg_id)
+
+
